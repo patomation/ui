e7c9864480ce7d4d9af1432cdba52d6ae4b198b9
patomation <patomation@gmail.com>
2019-08-17 15:46:30 +0700

    adds input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styles from './styles.js'
+
+export default (props) => {
+
+  const {
+    type, name, onChange, onBlur, value, error, placeholder,
+    className, onClick, background, color,
+    containerStyle, inputStyle, inputErrorStyle, errorStyle, style
+  } = props;
+
+
+
+  return(
+    <div
+      className={className}
+      style={{
+        ...styles.container,
+        ...style,
+        ...containerStyle
+      }}>
+
+      <input
+        type={type}
+        name={name}
+        onChange={onChange}
+        onBlur={onBlur}
+        placeholder={placeholder}
+        value={value}
+        onClick={onClick}
+        style={{
+          ...styles.input,
+          ...( background ? { background: background } : null ),
+          ...( color ? { color: color } : null ),
+          ...inputStyle,
+          ...( error ? (inputErrorStyle || styles.inputError) : null )
+        }}/>
+
+        <div
+          style={{
+            ...( error ? styles.error : null ),
+            ...( error ? errorStyle : null )
+          }}>
+
+          {error}
+
+        </div>
+
+
+      </div>
+    )
+}
---
+++ src/components/Input/styles.js
@@ -0,0 +1,33 @@
+import config from '../config.js';
+
+export default {
+  container:{
+    width: '100%',
+    display: 'block',
+  },
+  input:{
+    width: '100%',
+    display: 'block',
+    fontFamily: 'sans-serif',
+    fontSize: '1rem',
+    textAlign: 'center',
+    border: '1px solid rgba(0,0,0,0.25)',
+    padding: '0.5rem 1rem',
+    borderRadius: config.size.corners,
+    textDecoration: 'none',
+    margin: '0',
+    WebkitBoxSizing: 'border-box', /* Safari/Chrome, other WebKit */
+    MozBoxSizing: 'border-box',    /* Firefox, other Gecko */
+    boxSizing: 'border-box',       /* Opera/IE 8+ */
+  },
+  inputError: {
+    border: '1px solid red',
+  },
+  error:{
+    width: '100%',
+    display: 'block',
+    textAlign: 'center',
+    color: 'red',
+    padding: '0.5rem 0',
+  }
+}

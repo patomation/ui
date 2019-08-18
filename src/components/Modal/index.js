efa9417f90c32b4622b133e6414c8c7b0c8ed6bd
patomation <patomation@gmail.com>
2019-08-18 22:26:39 +0700

    Modal component added

---
+++ src/components/Modal/index.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react';
+import styles from './styles.js'
+import Panel from '../Panel'
+import Center from '../Center'
+import IconButton from '../IconButton'
+
+export default (props) => {
+
+  const {
+    show, onClose,
+    className, children,
+    background, color, style
+  } = props;
+
+  const [open, setOpen] = useState(show || false);
+
+  const close = () => {
+    setOpen(false)
+    if(onClose) onClose(false)
+  }
+
+  useEffect(()=>{
+    setOpen(show)
+  },[show]);
+
+  if(open){
+    return(
+      <div
+        className={ `modal ${className}` }
+        style={{
+          ...styles.container,
+          ...( background ? { background: background } : null ),
+          ...( color ? { color: color } : null ),
+          ...style
+        }}>
+
+        <Center>
+        <Panel style={styles.panel}>
+
+          <IconButton
+            onClick={close}
+            style={styles.close}
+            icon='close' />
+
+          {children}
+
+        </Panel>
+        </Center>
+
+      </div>
+    )
+  } else {
+    return null;
+  }
+
+}
---
+++ src/components/Modal/styles.js
@@ -0,0 +1,20 @@
+export default {
+  container:{
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    background: 'rgba(0,0,0,0.25)',
+  },
+  panel:{
+    background: '#ffffff',
+    padding: '2rem',
+  },
+  close: {
+    position: 'fixed',
+    top:'1.5rem',
+    right: '1.5rem'
+
+  }
+}

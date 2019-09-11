89c763ce4ca561353aee99fdd2f11b571a20ddd0
patomation <patomation@gmail.com>
2019-09-11 14:21:56 +0700

    v0.7.0 Cleans up React Scripts and simplifies module

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-import './style.scss';
-
-const moduleHotAccept = (mod) => {
-  if (mod && mod.hot) {
-    mod.hot.accept();
-  }
-}
-moduleHotAccept(module);
-
-const renderToDOM = (root) => {
-  if (root !== null) {
-    ReactDOM.render(<App />, root)
-  }
-}
-renderToDOM(document.getElementById('root'));
-
-export {
-  renderToDOM,
-  moduleHotAccept
-};
+export {default as Button} from './Button';
+export {default as Calendar} from './Calendar';
+export {default as Center} from './Center';
+export {default as Code} from './Code';
+export {default as Collapse} from './Collapse';
+export {default as Content} from './Content';
+export {default as DragDrop} from './DragDrop';
+export {default as Grid} from './Grid';
+export {default as Gutter} from './Gutter';
+export {default as Header} from './Header';
+export {default as Heading} from './Heading';
+export {default as Hr} from './Hr';
+export {default as Icon} from './Icon';
+export {default as IconButton} from './IconButton';
+export {default as Input} from './Input';
+export {default as Keyboard} from './Keyboard';
+export {default as Link} from './Link';
+export {default as List} from './List';
+export {default as Login} from './Login';
+export {default as Modal} from './Modal';
+export {default as Nav} from './Nav';
+export {default as NavItem} from './NavItem';
+export {default as Panel} from './Panel';
+export {default as SideBar} from './SideBar';
+export {default as SideBarItem} from './SideBarItem';
+export {default as Spinner} from './Spinner';
+export {default as User} from './User';

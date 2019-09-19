a8696fbfa95b254f335b0dfff41d2f7fcd27afd8
patomation <patomation@gmail.com>
2019-09-19 11:26:58 +0700

    Nav component simpolified to just be a top-bar and nothing more for now

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,40 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import styles from './styles.js'
 
-export default (props) => {
+export default ({
+  className, children,
+  background, color, style
+}) =>
 
-  const {
-    className, children, onClick,
-    background, color, style
-  } = props;
+<nav
+  className={className}
+  style={{
+    ...styles.container,
+    ...( background ? { background: background } : null ),
+    ...( color ? { color: color } : null ),
+    ...style
+  }}>
 
-  return(
-    <nav
-      onClick={onClick}
-      className={className}
-      style={{
-        ...styles.container,
-        ...( background ? { background: background } : null ),
-        ...( color ? { color: color } : null ),
-        ...style
-      }}>
+  {children}
 
-      {React.Children.map( children, (child, index) => {
-
-        return React.cloneElement( (child),{
-          ...{
-            style: styles.item,
-            children: (
-              <>
-                {child.props.children}
-                <div style={{
-                  ...styles.bottomBar
-                }}></div>
-              </>)},
-          ...( child.type !== 'a' ? { activeStyle:{opacity: '1'} } : null )
-        })
-      })}
-
-    </nav>
-  )
-}
+</nav>
--- src/Nav/styles.js
+++ src/Nav/styles.js
@@ -2,23 +2,9 @@ import config from '../config.js'
 
 export default {
   container:{
-
-  },
-  item:{
     color: '#ffffff',
-    marginRight: config.size.gutters,
-    position: 'relative',
-    paddingBottom: '0.5rem',
-    display: 'inline-block',
-    textDecoration: 'none',
-    opacity: '0.75'
-  },
-  bottomBar:{
-    // background: '#ffffff',
-    position: 'absolute',
-    bottom: '0',
-    width: '100%',
-    height: '4px',
-    borderRadius: `${config.size.corners} ${config.size.corners} 0 0`,
+    display: 'flex',
+    background: config.color.violet,
+    padding: config.size.gutters,
   }
 }

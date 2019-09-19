d9efc7e59489df72b5e885017b636af83190ee5c
patomation <patomation@gmail.com>
2019-09-19 11:20:44 +0700

    adds colors to config

--- src/config.js
+++ src/config.js
@@ -4,7 +4,12 @@ export default {
     gutters: '1rem',
     sideBar: '200px',
   },
-  color:{
+  color:{    
 
+    dust:    '#e2c2c6', //Lightest
+    tuscany: '#b9929f',
+    berry:   '#9c528b',
+    indigo:  '#610f7f',
+    violet:  '#2f0147', //Darkest
   }
 }

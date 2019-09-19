99e0f666519d954ce80708e90feb88b656836472
patomation <patomation@gmail.com>
2019-09-19 11:14:12 +0700

    Adds ImageGrid component

---
+++ src/ImageGrid/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './styles.js'
+
+import { Grid, Image } from '../'
+import config from '../config'
+import check from '../../utility/check'
+
+const ImageGrid = ({
+  className, children,
+  background, color, style,
+  col, row, gap,
+}) =>
+
+<Grid
+  className={className || 'image-grid'}
+  style={{
+    ...styles.container,
+    ...( background ? { background: background } : null ),
+    ...( color ? { color: color } : null ),
+    ...style
+  }}
+  col={col}
+  row={row}
+  gap={gap || config.size.gutters}>
+
+  {React.Children.map(children, (child, index) => {
+    if( check(child, 'props.children[0].props.src') ) {
+      return <Image src={child.props.children[0].props.src} square />
+    }
+  }
+  )}
+
+</Grid>
+
+export default ImageGrid

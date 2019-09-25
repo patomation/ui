aed8f161231db52a27048b579250d9a51dbc64a8
patomation <patomation@gmail.com>
2019-09-25 10:11:55 +0700

    Clickable component added

---
+++ src/Clickable/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles.js'
+
+const Clickable = ({
+  className, children,
+  onClick,
+  background, color, style
+}) =>
+
+<div
+  onClick={onClick}
+  className={className}
+  style={{
+    ...styles.container,
+    ...( background ? { background: background } : null ),
+    ...( color ? { color: color } : null ),
+    ...style
+  }}>
+
+  {children}
+
+</div>
+
+Clickable.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+  onClick: PropTypes.func,
+  background: PropTypes.string,
+  color: PropTypes.string,
+  style: PropTypes.object
+}
+
+
+export default Clickable
---
+++ src/Clickable/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import Clickable from './index.js';
+
+describe('<Clickable />', () => {
+
+  it("renders", () => {
+    mount(<Clickable />)
+  });
+
+});

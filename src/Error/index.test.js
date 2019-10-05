76584b1d55836ec18216cc48868a00f77c6f4412
patomation <patomation@gmail.com>
2019-10-05 16:37:09 +0700

    feature: Error component added

---
+++ src/Error/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import Error from './index.js'
+
+describe('<Error />', () => {
+  it('renders', () => {
+    mount(<Error />)
+  })
+})

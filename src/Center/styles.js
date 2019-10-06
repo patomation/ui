5616ef68b25532b8c5213a5bd8d2780810be06fe
patomation <patomation@gmail.com>
2019-10-06 13:08:36 +0700

    v0.12.3 - fix: iso webkit transform for Center component

--- src/Center/styles.js
+++ src/Center/styles.js
@@ -7,6 +7,8 @@ export default {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%,-50%)',
+    WebkitTransform: 'translate(-50%,-50%)',
+    MsTransform: ' translate(-50%,-50%)',
     padding: '1rem'
   }
 }

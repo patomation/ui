9fd05008eaebd2b04026985fedaf8e0438011a82
patomation <patomation@gmail.com>
2019-10-26 10:47:58 +0700

    fix: Header padding

--- src/Header/styles.js
+++ src/Header/styles.js
@@ -4,7 +4,7 @@ export default {
   container: {
     paddingBottom: '1px',
     backgroundColor: '#597081',
-    padding: `0 ${config.size.gutters}`,
+    padding: `${config.size.gutters}`,
     paddingTop: config.size.gutters,
     flex: '0 1 auto'
   }

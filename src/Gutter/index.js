a18e8ae84d2f4c5570e633db45cf5b7e7765ca1a
patomation <patomation@gmail.com>
2019-12-11 23:43:47 +0700

    feature: Gutter gets half prop for half the gutter

--- src/Gutter/index.js
+++ src/Gutter/index.js
@@ -2,20 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.js'
 import concat from '../_utility/concat.js'
+import extract from '../_utility/extract.js'
+import config from '../config.js'
 /**
 * A spacer component that adds a standard amount of padding between things
 */
 const Gutter = ({
-  className, style, disabled, vertical
+  className, style, disabled, vertical, half
 }) => {
   const Tag = vertical ? 'span' : 'div'
+
+  const { number, unit } = extract(config.size.gutters)
+  const padding = `${number * (half === true ? 0.5 : 1) }${unit}`
 
   return disabled !== true
     ? <Tag
       className={concat('gutter', className)}
       style={{
-        ...styles.horizontal,
-        ...(vertical === true ? styles.vertical : null),
+        ...(vertical === true
+          ? {
+            ...styles.vertical,
+            paddingLeft: padding
+          }
+          : {
+            ...styles.horizontal,
+            paddingBottom: padding
+          }),
         ...style
       }}>
     </Tag>
@@ -28,8 +40,18 @@ Gutter.propTypes = {
   **/
   className: PropTypes.string,
   style: PropTypes.object,
+  /**
+  * A way to disable the padding style without having to remove the element
+  **/
   disabled: PropTypes.bool,
-  vertical: PropTypes.bool
+  /**
+  * Vertical gutter spacing option
+  **/
+  vertical: PropTypes.bool,
+  /**
+  * devides the gutter amount in half
+  **/
+  half: PropTypes.bool
 }
 
 export default Gutter
--- src/Gutter/styles.js
+++ src/Gutter/styles.js
@@ -1,12 +1,7 @@
-import config from '../config.js'
-
 export default {
   horizontal: {
-    paddingBottom: config.size.gutters
   },
   vertical: {
-    paddingLeft: config.size.gutters,
-    paddingBottom: 0,
     display: 'inline-block'
   }
 }

b76764af094b2ebdfafc3887ee20b0de7c8f258f
patomation <patomation@gmail.com>
2019-12-11 23:46:01 +0700

    feature: Card: Gets animation and better props

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,48 +1,119 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.js'
 import concat from '../_utility/concat.js'
-import { Panel, Image } from '../'
+import posed from 'react-pose'
+import { Panel, Image, Icon, Shape, Gutter, Center } from '../'
+
+const Box = posed.div({
+  hover: {
+    transform: 'translateY(-3px)',
+    transition: { type: 'spring', stiffness: 100 }
+  },
+  nohover: {
+    transform: 'translateY(0px)',
+    transition: { type: 'spring', stiffness: 100 }
+ }
+})
+
 /**
 * A ui card component that displays an image title and text
 */
 const Card = ({
   className,
-  children, middle, bottom,
   background, color, style,
-  image, alt
-}) =>
+  image, alt = '', icon,
+  title, description, footer, children,
+  onClick
+}) => {
+
+  const [ hover, setHover ] = useState(false)
+
+  return (
+  <Box
+    className={concat('card', className)}
+    style={{display: 'flex'}}
+    pose={ hover ? "hover" : "nohover" }
+    onClick={onClick} >
 
   <Panel
-    className={concat('card', className)}
     style={{
       ...styles.container,
+      cursor: 'pointer',
       ...(background ? { background: background } : null),
       ...(color ? { color: color } : null),
       ...style
     }}
     containerStyle={{ margin: 0 }}
-    contentStyle={styles.content}>
+    contentStyle={styles.content}
+    onMouseEnter={() => { setHover(true) }}
+    onMouseLeave={() => { setHover(false) }}>
 
-    <Image className='card__image' src={image} alt={alt} rectangle />
+    { image
+      ? <Image
+        className='card__image'
+        src={image}
+        alt={alt}
+        rectangle />
+      : <Shape rectangle background='gray'>
+          { icon
+            ? <Center style={{textAlign:'center'}}>
+                <Icon name={icon} responsive style={{width:'33%'}} />
+              </Center>
+            : null
+          }
+        </Shape>
+    }
 
-    { middle || children
+
+    <div style={{
+      padding:'1rem'
+    }}>
+
+      { title
+        ? <h3
+            style={{
+              textDecoration: hover ? 'underline' : 'none'
+            }}>
+            { title }
+          </h3>
+
+        : <div
+            style={{
+              background: 'gray',
+              height: '1rem'
+            }}></div>
+      }
+      <Gutter half/>
+      { description
+        ? <p>{ description }</p>
+        : <>
+          <div
+            style={{
+              background: 'silver',
+              height: '0.75rem'
+            }}></div>
+          <Gutter half/>
+          <div
+            style={{
+              background: 'silver',
+              height: '0.75rem'
+            }}></div></>
+        }
+
+    </div>
+
+    { footer || children
       ? <div
-        className='card__middle'
-        style={styles.middle}>
-        {middle || children}
-      </div>
-      : null }
-
-    {bottom
-      ? <div
-        className='card__bottom'
-        style={styles.bottom}>
-        {bottom}
+        className='card__footer'
+        style={styles.footer}>
+        {footer || children}
       </div>
       : null }
 
   </Panel>
+  </Box>
+)}
 
 Card.propTypes = {
   /**
@@ -55,8 +126,18 @@ Card.propTypes = {
   background: PropTypes.string,
   color: PropTypes.string,
   style: PropTypes.object,
+  /**
+  * Card image at the top
+  **/
   image: PropTypes.string,
-  alt: PropTypes.string
+  /**
+  * Image alt text
+  **/
+  alt: PropTypes.string,
+  /**
+  * an icon can be used instead of using an image
+  **/
+  icon: PropTypes.string
 }
 
 export default Card

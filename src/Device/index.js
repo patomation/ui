f4e6d76fb757e85cce32b4a9294321d2bdfba5d0
patomation <patomation@gmail.com>
2019-12-29 21:43:38 +0700

    feature: Device - A basic device for displaying demos

---
+++ src/Device/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles.js'
+
+import { Shape } from '../'
+
+const Device = ({
+  children,
+  width, maxWidth,
+  background, color, style
+}) =>
+
+  <div
+    className={'device'}
+    style={{
+      ...styles.container,
+      ...(background ? { background: background } : null),
+      ...(color ? { color: color } : null),
+      position: 'relative',
+      width,
+      ...(maxWidth ? {
+        maxWidth,
+        margin: '0 auto',
+      } : null),
+      ...style
+    }}>
+    <div className='device__frame' style={{
+      overFlow: 'hidden',
+      borderRadius: '1rem',
+      border: '1px solid silver',
+      boxSizing: 'border-box',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      height: '100%',
+      width: '100%',
+      zIndex: '5',
+      pointerEvents: 'none'
+    }}>
+
+    </div>
+    <Shape
+      className={'device__screen'}
+      portrait
+      background='silver'
+      style={{
+        padding: '1rem',
+      }}>
+
+      {children}
+
+    </Shape>
+    <Shape className='device__button' circle background='silver' width='10%' style={{paddingBottom: '1rem'}} />
+  </div>
+
+Device.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+  background: PropTypes.string,
+  color: PropTypes.string,
+  width: PropTypes.string,
+  maxWidth: PropTypes.string,
+  style: PropTypes.object
+}
+
+export default Device
---
+++ src/Device/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import Device from './index.js'
+
+describe('<Device />', () => {
+  it('renders', () => {
+    mount(<Device />)
+  })
+})

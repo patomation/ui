936381cb8dcf0ccea8a098b2d1d3a59df1b3644e
patomation <patomation@gmail.com>
2019-12-29 21:41:00 +0700

    feature: Syntax highlighting with highlight.js

--- src/Code/index.js
+++ src/Code/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import styles from './styles.js'
 import concat from '../_utility/concat.js'
 import { Panel } from '../'
+import hljs from 'highlight.js'
 
 /**
 * A component for displaying code samples
@@ -10,23 +11,131 @@ import { Panel } from '../'
 const Code = ({
   className, block,
   children,
-  background = 'gray', color = '#ffffff', style
+  background = '#333', color = '#ffffff', style
 }) => {
+
+  if(block) console.log('BLOCK', hljs.highlightAuto('<span>Hello World!</span>').value )
+
   return (
-    <Panel
+    <div
       className={concat('code', className)}
       style={{
         ...styles.container,
         ...(background ? { background: background } : null),
         ...(color ? { color: color } : null),
+        borderRadius: '0.5rem',
         ...style
       }}>
-      <code>
+      <style>{`
+        /* a11y-dark theme */
+        /* Based on the Tomorrow Night Eighties theme: https://github.com/isagalaev/highlight.js/blob/master/src/styles/tomorrow-night-eighties.css */
+        /* @author: ericwbailey */
 
-        {children || block}
+        /* Comment */
+        .hljs-comment,
+        .hljs-quote {
+          color: #d4d0ab;
+        }
+
+        /* Red */
+        .hljs-variable,
+        .hljs-template-variable,
+        .hljs-tag,
+        .hljs-name,
+        .hljs-selector-id,
+        .hljs-selector-class,
+        .hljs-regexp,
+        .hljs-deletion {
+          color: #ffa07a;
+        }
+
+        /* Orange */
+        .hljs-number,
+        .hljs-built_in,
+        .hljs-builtin-name,
+        .hljs-literal,
+        .hljs-type,
+        .hljs-params,
+        .hljs-meta,
+        .hljs-link {
+          color: #f5ab35;
+        }
+
+        /* Yellow */
+        .hljs-attribute {
+          color: #ffd700;
+        }
+
+        /* Green */
+        .hljs-string,
+        .hljs-symbol,
+        .hljs-bullet,
+        .hljs-addition {
+          color: #abe338;
+        }
+
+        /* Blue */
+        .hljs-title,
+        .hljs-section {
+          color: #00e0e0;
+        }
+
+        /* Purple */
+        .hljs-keyword,
+        .hljs-selector-tag {
+          color: #dcc6e0;
+        }
+
+        .hljs {
+          display: block;
+          overflow-x: auto;
+          /* background: #2b2b2b; */
+          color: #f8f8f2;
+          padding: 0.5em;
+        }
+
+        .hljs-emphasis {
+          font-style: italic;
+        }
+
+        .hljs-strong {
+          font-weight: bold;
+        }
+
+        @media screen and (-ms-high-contrast: active) {
+          .hljs-addition,
+          .hljs-attribute,
+          .hljs-built_in,
+          .hljs-builtin-name,
+          .hljs-bullet,
+          .hljs-comment,
+          .hljs-link,
+          .hljs-literal,
+          .hljs-meta,
+          .hljs-number,
+          .hljs-params,
+          .hljs-string,
+          .hljs-symbol,
+          .hljs-type,
+          .hljs-quote {
+                color: highlight;
+            }
+
+            .hljs-keyword,
+            .hljs-selector-tag {
+                font-weight: bold;
+            }
+        }
+
+
+        `}</style>
+      <pre>
+      <code className='hljs'>
+        <div dangerouslySetInnerHTML={{ __html: hljs.highlightAuto(children || block).value }} />
 
       </code>
-    </Panel>
+      </pre>
+    </div>
   )
 }
 

c8f99d03eca1c7248c5d79c4d6cb3f00fe64f380
patomation <patomation@gmail.com>
2019-12-29 21:43:56 +0700

    feature: Placeholder added

---
+++ src/Placeholder/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles.js'
+
+console.log(new Array(10));
+
+const Placeholder = ({
+  background, color, style, lines = 1
+}) =>
+
+  <div
+    className={'placeholder'}
+    style={{
+      ...styles.container,
+      ...(background ? { background: background } : null),
+      ...(color ? { color: color } : null),
+      ...style
+    }}>
+    { new Array(lines).fill(0).map( (_, index) => {
+      console.log(index);
+      return <div
+        className={'placeholder__line'}
+        key = {'line' + index}
+        style={{
+          background: 'rgba(255,255,255,0.4)',
+          height: '0.5rem',
+          marginBottom: '0.5rem',
+        }}></div>
+    })}
+
+  </div>
+
+Placeholder.propTypes = {
+  background: PropTypes.string,
+  color: PropTypes.string,
+  style: PropTypes.object,
+  lines: PropTypes.number
+}
+
+export default Placeholder
---
+++ src/Placeholder/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import Placeholder from './index.js'
+
+describe('<Placeholder />', () => {
+  it('renders', () => {
+    mount(<Placeholder />)
+  })
+})

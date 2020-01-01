83a519ed99e3927059f55ddaa540f789c4d61a83
patomation <patomation@gmail.com>
2020-01-01 14:05:24 +0700

    fix: Loader test - uses act() and jest timers for async tests

--- src/Loader/index.test.js
+++ src/Loader/index.test.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import ReactDOM from 'react-dom'
+/**
+* How act() works
+* https://reactjs.org/docs/test-utils.html#act
+**/
+import { act } from 'react-dom/test-utils'
 
 import Loader from './index.js'
 
+let container
+
+const handleComplete = jest.fn()
+
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  document.body.removeChild(container)
+  container = null
+})
+
 describe('<Loader />', () => {
   it('renders', () => {
-    mount(<Loader />)
+    act(() => {
+      jest.useFakeTimers()
+      ReactDOM.render(<Loader onComplete={handleComplete} progressBar={true} progressBarTime={50}/>, container)
+      jest.runAllTimers()
+    })
+    expect(handleComplete).toBeCalled()
   })
 })

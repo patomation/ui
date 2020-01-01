09f4f8f83f93dd392ad1fe265a67027d09325887
patomation <patomation@gmail.com>
2020-01-01 11:00:37 +0700

    feature: Range component added

---
+++ src/Range/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import Slider from './index.js'
+
+describe('<Slider />', () => {
+  it('renders', () => {
+    mount(<Slider />)
+  })
+})

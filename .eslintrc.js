1271aaac1a69efbbcc558b1a40167ba49d2dee30
patomation <patomation@gmail.com>
2020-03-01 22:14:54 +0700

    feat: better build tools and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   env: {
     browser: true,
@@ -6,28 +7,33 @@ module.exports = {
     node: true
   },
   extends: [
-    'standard',
+    'standard-with-typescript',
     'plugin:react/recommended'
   ],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
+  // parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: './tsconfig.json',
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 2018,
     sourceType: 'module'
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   plugins: [
     'react'
   ],
+  // globals: {
+  //   Atomics: 'readonly',
+  //   SharedArrayBuffer: 'readonly'
+  // },
   rules: {
+    '@typescript-eslint/restrict-template-expressions': 0, // Turning this off for now.
+    '@typescript-eslint/strict-boolean-expressions': 0, // Turning this off for now.
+    'react/prop-types': 0, // will be generating proptypes from typescript
   }
 }
